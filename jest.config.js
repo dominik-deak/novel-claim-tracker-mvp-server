0220d4657ecb9adbe24af398d7b4d4c34de46074
Dominik Deak <dominik.deak@protonmail.com>
2025-11-11 23:27:12 +0000

    set up jest & aws-sdk-client-mock

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+/** @type {import('jest').Config} */
 module.exports = {
+	preset: "ts-jest",
 	testEnvironment: "node",
 	roots: ["<rootDir>/test"],
 	testMatch: ["**/*.test.ts"],
-	transform: {
-		"^.+\\.tsx?$": "ts-jest",
-	},
+	collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/mock-server/**"],
+	coverageDirectory: "coverage",
+	coverageReporters: ["text", "lcov", "html"],
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	verbose: true,
 };
