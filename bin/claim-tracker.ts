fed6a685990a98bd63d3a66bc4b039d90f20c44c
Dominik Deak <dominik.deak@protonmail.com>
2025-11-11 13:14:16 +0000

    initialise cdk app

---
+++ bin/claim-tracker.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { ClaimTrackerStack } from '../lib/claim-tracker-stack';
+
+const app = new cdk.App();
+new ClaimTrackerStack(app, 'ClaimTrackerStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'eu-west-2'
+  }
+});
---
+++ lib/claim-tracker-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class ClaimTrackerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Infrastructure will be defined in Phase 1, Step 2
+  }
+}
